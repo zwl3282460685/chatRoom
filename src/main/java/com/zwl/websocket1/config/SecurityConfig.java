dff1f6d26dc95c160fa36ec3702cb46345bef801
zwl3242460685 <1312480826@qq.com>
2020-11-23 21:00:08 +0800

    增加点对点聊天的功能

---
+++ src/main/java/com/zwl/websocket1/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.zwl.websocket1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("javaboy")
+                .password("123")
+                .roles("admin")
+                .and()
+                .withUser("sang")
+                .password("123")
+                .roles("user");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().permitAll();
+    }
+}
--- src/main/java/com/zwl/websocket1/controller/GreetingController.java
+++ src/main/java/com/zwl/websocket1/controller/GreetingController.java
@@ -1,16 +1,35 @@
 package com.zwl.websocket1.controller;
 
+import com.zwl.websocket1.bean.Chat;
 import com.zwl.websocket1.bean.Message;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+
+import java.security.Principal;
 
 @Controller
 public class GreetingController {
 
+    @Autowired
+    SimpMessagingTemplate simpMessagingTemplate;
+
+//    @MessageMapping("/hello")
+//    @SendTo("/topic/greetings")
+//    public Message greeting(Message message){
+//        return message;
+//    }
+
     @MessageMapping("/hello")
-    @SendTo("/topic/greetings")
-    public Message greeting(Message message){
-        return message;
+    public void greeting(Message message){
+        simpMessagingTemplate.convertAndSend("/topic/greetings", message);
+    }
+
+    @MessageMapping("/chat")
+    public void chat(Principal principal, Chat chat){
+        chat.setFrom(principal.getName());
+        simpMessagingTemplate.convertAndSendToUser(chat.getTo(), "/queue/chat", chat);
     }
 }
